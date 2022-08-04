73ab1023ac48aea3d853865ab40e66fca173f2da
waterbean54 <tnqls8238@naver.com>
2022-08-04 15:40:36 +0900

    1장 완료

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceTest.java
@@ -0,0 +1,36 @@
+package hello.advanced.trace.hellotrace;
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+class HelloTraceTest {
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+    @Test
+    void begin_end_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+    @Test
+    void begin_exception_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+
+}
