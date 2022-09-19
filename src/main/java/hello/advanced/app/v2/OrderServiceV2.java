c8609bfddd1d78dd462460bbaba87ca401679baf
waterbean <waterbean54@gmail.com>
2022-09-20 08:36:45 +0900

    test

--- src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -14,6 +14,7 @@ public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
     private final HelloTraceV2 trace;
+    private String b;
 
     public void orderItem(TraceId traceId, String itemId) {
         TraceStatus status = null;
