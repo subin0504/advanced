14d9c0150062146e56175a5ba987ec1059a0679e
subin <waterbean54@gmail.com>
2022-09-28 20:04:09 +0900

    fix:자기소개 페이지 등록 버그 수정(#2)
    자기소개 페이지에서 등록이 안되는 버그 수정

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class HelloTraceV1 {
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
-    private static final String EX_PREFIX = "<X-";
+    private static final String EX_PREFIX = "<X---";
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
